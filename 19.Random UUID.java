6bf02c27fa34d68ca0d1767fdeb95f94c7be82d1
Sudharsen-jerry <128677099+Sudharsen-jerry@users.noreply.github.com>
2024-02-22 14:43:34 +0530

    Delete 19.Random UUID.java

--- 19.Random UUID.java
+++
@@ -1,7 +0,0 @@
-import java.util.UUID;
-public class UUIDExample {
-    public static void main(String[] args) {
-        UUID uuid = UUID.randomUUID();
-        System.out.println("Random UUID: " + uuid);
-    }
-}
